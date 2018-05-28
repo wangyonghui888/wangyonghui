a93cf924d35d20aa2a74d6e28289e6d35e417c08
wangyonghui <puchengerguai@163.com>
2018-05-28 23:54:04 +0800

    第一次提交全部文件

---
+++ src/main/java/wang/yong/hui/HuiApplication.java
@@ -0,0 +1,12 @@
+package wang.yong.hui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuiApplication.class, args);
+	}
+}
